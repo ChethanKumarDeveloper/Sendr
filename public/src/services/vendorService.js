030149dfd80e54adde07be9088343dd44e45e133
ChethanKumarDeveloper <chethan22ec011@drttit.edu.in>
2025-11-29 07:44:13 +0530

    Update vendorService.js

--- public/src/services/vendorService.js
+++ public/src/services/vendorService.js
@@ -1,1 +1,17 @@
 
+// src/services/vendorService.js
+import { doc, getDoc, setDoc, addDoc, collection } from "firebase/firestore";
+import { db } from "../firebase";
+
+export async function getVendor(uid) {
+  const d = await getDoc(doc(db, "vendors", uid));
+  return d.exists() ? d.data() : null;
+}
+
+export async function createShop(shop) {
+  return await addDoc(collection(db, "shops"), shop);
+}
+
+export async function setVendorProfile(uid, data) {
+  return await setDoc(doc(db, "vendors", uid), data);
+}
